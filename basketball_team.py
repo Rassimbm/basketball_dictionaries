fa24e0531793f184bc30d727e974b9a0ac693346
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2023-11-02 02:04:56 -0500

    Bonus challenge: Added a @class method that returns a new list of Player objects.

--- basketball_team.py
+++ basketball_team.py
@@ -10,51 +10,53 @@ class Player:
         player_infos = f"Name: {self.name} \nAge: {self.age} \nPosition: {self.position} \nTeam: {self.team}"
         return player_infos
     
-"""
-Given the example list of players, write a for loop that will populate an empty list with Player objects from the original list of dictionaries.
-"""
+    """
+    Add an @class method called get_team(cls, team_list) that, given a list of dictionaries populates and returns a new list of Player objects.
+    """
+    @classmethod
+    def get_team(cls, list):
+        team = []
+        for player_dict in list:
+            team.append(cls(player_dict))
+        return team
 
 players = [
-{
-    "name": "Kevin Durant", 
-    "age":34, 
-    "position": "small forward", 
-    "team": "Brooklyn Nets"
-},
-{
-    "name": "Jason Tatum", 
-    "age":24, 
-    "position": "small forward", 
-    "team": "Boston Celtics"
-},
-{
-    "name": "Kyrie Irving", 
-    "age":32,
-    "position": "Point Guard", 
-    "team": "Brooklyn Nets"
-},
-{
-    "name": "Damian Lillard", 
-    "age":33,
-    "position": "Point Guard", 
-    "team": "Portland Trailblazers"
-},
-{
-    "name": "Joel Embiid", 
-    "age":32,
-    "position": "Power Foward", 
-    "team": "Philidelphia 76ers"
-},
-{
-    "name": "", 
-    "age":16, 
-    "position": "P", 
-    "team": "en"
-}
+            {
+                "name": "Kevin Durant", 
+                "age":34, 
+                "position": "small forward", 
+                "team": "Brooklyn Nets"
+            },
+            {
+                "name": "Jason Tatum", 
+                "age":24, 
+                "position": "small forward", 
+                "team": "Boston Celtics"
+            },
+            {
+                "name": "Kyrie Irving", 
+                "age":32,
+                "position": "Point Guard", 
+                "team": "Brooklyn Nets"
+            },
+            {
+                "name": "Damian Lillard", 
+                "age":33,
+                "position": "Point Guard", 
+                "team": "Portland Trailblazers"
+            },
+            {
+                "name": "Joel Embiid", 
+                "age":32,
+                "position": "Power Foward", 
+                "team": "Philidelphia 76ers"
+            },
+            {
+                "name": "", 
+                "age":16, 
+                "position": "P", 
+                "team": "en"
+            }
 ]
-
-new_team = []
-for player_dict in players:
-    new_team.append(Player(player_dict))
-        
-print(f"Check the new team: \n{new_team}")
+team_1 = Player.get_team(players)
+print(team_1)
