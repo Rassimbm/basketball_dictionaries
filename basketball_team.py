08711dad084af0d67d18e2eb6c9a4b4297d94ed2
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2023-11-02 01:38:25 -0500

    Challenge 3: Make a list of Player instances from a list of dictionaries

--- basketball_team.py
+++ basketball_team.py
@@ -1,44 +1,60 @@
-"""
-Update the constructor to accept a dictionary with a single player's information instead of individual arguments for the attributes.
-"""
 class Player:
     def __init__(self, player_dict):
         self.name = player_dict["name"]
         self.age = player_dict["age"]
         self.position = player_dict["position"]
         self.team = player_dict["team"]
+        self.all_players = []
 
     def __repr__(self):
         player_infos = f"Name: {self.name} \nAge: {self.age} \nPosition: {self.position} \nTeam: {self.team}"
         return player_infos
-
-"""
-Given these variables, create Player instances for each of the following dictionaries. Be sure to instantiate these outside the class definition, in the outer scope.
-"""
-kevin = {
-        "name": "Kevin Durant", 
-        "age":34, 
-        "position": "small forward", 
-        "team": "Brooklyn Nets"
-        }
-jason = {
-        "name": "Jason Tatum", 
-        "age":24, 
-        "position": "small forward", 
-        "team": "Boston Celtics"
-        }
-kyrie = {
-        "name": "Kyrie Irving", 
-        "age":32, 
-        "position": "Point Guard", 
-        "team": "Brooklyn Nets"
-        }
     
-# Create your Player instances here!
-player_kevin = Player(kevin)
-print(player_kevin)
-player_jason = Player(jason)
-print(player_jason)
-player_kyrie = Player(kyrie)
-print(player_kyrie)
+"""
+Given the example list of players, write a for loop that will populate an empty list with Player objects from the original list of dictionaries.
+"""
 
+players = [
+{
+    "name": "Kevin Durant", 
+    "age":34, 
+    "position": "small forward", 
+    "team": "Brooklyn Nets"
+},
+{
+    "name": "Jason Tatum", 
+    "age":24, 
+    "position": "small forward", 
+    "team": "Boston Celtics"
+},
+{
+    "name": "Kyrie Irving", 
+    "age":32,
+    "position": "Point Guard", 
+    "team": "Brooklyn Nets"
+},
+{
+    "name": "Damian Lillard", 
+    "age":33,
+    "position": "Point Guard", 
+    "team": "Portland Trailblazers"
+},
+{
+    "name": "Joel Embiid", 
+    "age":32,
+    "position": "Power Foward", 
+    "team": "Philidelphia 76ers"
+},
+{
+    "name": "", 
+    "age":16, 
+    "position": "P", 
+    "team": "en"
+}
+]
+
+new_team = []
+for player_dict in players:
+    new_team.append(Player(player_dict))
+        
+print(f"Check the new team: \n{new_team}")
