e0a80558d64a9f4bf28e63ffa3384485b6459e59
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2023-11-01 22:19:10 -0500

    Challenge 1: Update the constructor to accept a dictionary with a single player's information instead of individual arguments for the attributes.

--- basketball_team.py
+++ basketball_team.py
@@ -1,6 +1,9 @@
+"""
+Update the constructor to accept a dictionary with a single player's information instead of individual arguments for the attributes.
+"""
 class Player:
-    def __init__(self, name, age, position, team):
-        self.name = name
-        self.age = age
-        self.position = position
-        self.team = team
+    def __init__(self, player_dict):
+        self.name = player_dict["name"]
+        self.age = player_dict["age"]
+        self.position = player_dict["position"]
+        self.team = player_dict["team"]
